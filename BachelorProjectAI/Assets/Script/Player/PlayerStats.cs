603c6067824284addf57b8d7463df1f5c7774557
Morten Skov <mort79e5@student.eadania.dk>
2022-05-02 07:13:01 +0200

    Health bar UI and logic added

---
+++ BachelorProjectAI/Assets/Script/Player/PlayerStats.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int healthLevel = 10;
+    public int maxHealth;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = SetMaxHealthFromHealthLevel();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private int SetMaxHealthFromHealthLevel()
+    {
+        maxHealth = healthLevel * 10;
+        return maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetCurrentHealth(currentHealth);
+    }
+
+
+}
