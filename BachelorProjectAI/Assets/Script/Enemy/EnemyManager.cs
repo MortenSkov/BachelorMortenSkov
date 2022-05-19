aa470cc166043f0dad54b21da7db081454207e28
Morten Skov <mort79e5@student.eadania.dk>
2022-05-19 11:24:49 +0200

    Added beginning scripts and functionality for lock on mechanic

--- BachelorProjectAI/Assets/Script/Enemy/EnemyManager.cs
+++ BachelorProjectAI/Assets/Script/Enemy/EnemyManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterManager : MonoBehaviour
+public class EnemyManager : CharacterManager
 {
-    public Transform lockOnTransform;
+    
 }
