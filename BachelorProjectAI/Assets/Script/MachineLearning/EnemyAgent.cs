eb2237dc9ca3b5e36d6fdff1da8ded013cd52a16
Morten Skov <mort79e5@student.eadania.dk>
2022-05-25 14:10:09 +0200

    Machine learning logic added, agent can now train in editor

--- BachelorProjectAI/Assets/Script/MachineLearning/EnemyAgent.cs
+++ BachelorProjectAI/Assets/Script/MachineLearning/EnemyAgent.cs
@@ -3,14 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class EnemyAgent : Agent
 {
-    public override void OnActionReceived(ActionBuffers actions)
+    [SerializeField]
+    private Transform targetTransform;
+    [SerializeField]
+    private Material winMaterial;
+    [SerializeField]
+    private Material loseMaterial;
+    [SerializeField]
+    private MeshRenderer floorMeshRenderer;
+
+    public override void OnEpisodeBegin()
     {
-        Debug.Log(actions.DiscreteActions[0]);
+        transform.localPosition = Vector3.zero;
     }
 
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
 
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 3f;
+        transform.localPosition += moveSpeed * Time.deltaTime * new Vector3(moveX, 0, moveZ);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut) // modifies the actions that will then be received by the 'OnActionReceived' method
+    {
+        //base.Heuristic(actionsOut);
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            SetReward(+1f);
+            floorMeshRenderer.material = winMaterial;
+            EndEpisode();
+        }
+        if (other.CompareTag("Wall"))
+        {
+            SetReward(-1f);
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
+        }
+    }
 
 }
